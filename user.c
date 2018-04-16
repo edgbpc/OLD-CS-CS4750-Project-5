63a3306b1dd5076c6bebbc1f4de8a99b450d8013
Eric Goodwin <edgbpc@mail.umsl.edu>
2018-04-16 11:37:29 -0500

    turn in

--- user.c
+++ user.c
@@ -4,12 +4,12 @@ unsigned int * simClock;
 descriptor *resourceTable;
 
 
-int FindResource(int location, int value);
+int findResource(int location, int value);
 int main (int argc, char *argv[]){
-	printf("USER %d has launced\n", getpid());
+	printf("USER %d has launched\n", getpid());
 	
-
 	int myPID = getpid();
+
 	int randomTerminateConstant = 90; //change this to increase or decrease chance process will terminate.  currently 50% to terminate
 	int randomRequestConstant = 50;
 	//seed random number generator
@@ -22,14 +22,14 @@ int main (int argc, char *argv[]){
 	}
 	//Shared Memory Keys
 	keySimClock = 59566;
-	semaphoreKey = 59567;
+	//semaphoreKey = 59567;
 	descriptorKey = 59568;
 	//Get Shared Memory
-	if ((shmidSimClock = shmget(keySimClock, SHM_SIZE, 0666)) == -1){
+	if ((shmidSimClock = shmget(keySimClock, (2*(sizeof(unsigned int))), 0666)) == -1){
 		perror("user: could not get shmidSimClock\n");
 		return 1;
 	}
-	if ((shmidDescriptor = shmget(descriptorKey, SHM_SIZE, 0666)) == -1){
+	if ((shmidDescriptor = shmget(descriptorKey, (sizeof(descriptor)), 0666)) == -1){
 		perror("user: could not get shmidSimClock\n");
 		return 1;
 	}
@@ -45,77 +45,92 @@ int main (int argc, char *argv[]){
 		perror("user: failed to acceess parent message box");
 	}
 
+	int location = atoi(argv[1]);
+	//save location in the resource table to the process structure
+	(*resourceTable).process[location].RTLocation = location;
+	//copy maxCLaims to the process structure
+	int loop;
+	for(loop = 0; loop < maxResources; loop++){
+		(*resourceTable).process[location].maxClaimsAllowed[loop] = (*resourceTable).maxCanRequest[location][loop];
+	}
 
 	int counter = 0;
-	int location = atoi(argv[1]);
 	bool requestWasGranted = false;
 	int index; //for incrementing in loops
 
 	int claim;
 	
-	printf("USer process %d is located in index of %d of the resource table\n", getpid(), location);
+	//start as not blocked
+	(*resourceTable).process[location].blocked = false;
+
 	//while loop runs contiously but on first run, the IF statement fails
 	//and then suspends on waiting for a message. 
 	//message received and then can process the IF statement
 	while (1){
-		msgrcv(messageBoxID, &message, sizeof(message), myPID, IPC_NOWAIT);
-		requestWasGranted = message.granted;
+	if ((*resourceTable).process[location].blocked == false){ 
 		int willRequest = (rand() % 100) + 1;
 
+//		int willRequest = 100;
 		if (requestWasGranted == true){				
 			int willTerminate = (rand() % 100) + 1;
 			if (willTerminate >= randomTerminateConstant){
 				printf("USER %d is going to terminate\n", getpid());
 				//notify OSS that process is terminating
 				message.terminate = true; 
-				message.mesg_type = getppid();
+				message.mesg_type = 5;
 				//set location of process in resourceTable in message to OSS
-				//zero out allocated resources
-				for (index = 0; index < maxResources; index++){
-					(*resourceTable).allocated[location][index] = 0;
-				}
+				//zero out allocated resource
 				//send message to OSS
 				if(msgsnd(messageBoxID, &message, sizeof(message), 0) ==  -1){
 					perror("oss: failed to send message to user");
 				}
 				//process exits
-				exit(1); 
+				exit(EXIT_SUCCESS); 
 			}
+			requestWasGranted = false; //change flag back to false
 		} //end if(requestWasGranted == true)  block	
  
 		if (willRequest >= randomRequestConstant){	
-				claim = rand () % 20 + 1; //offset by one so I can use 0 as a sentry value in OSS.
+				claim = rand () % 20;
 				int newAllocation =  1 + (*resourceTable).allocated[location][claim];
 				
-				if (newAllocation <= (*resourceTable).maxCanRequest[location][claim]){
-					message.mesg_type = getppid();
-					message.request = claim;
+				if (newAllocation <= (*resourceTable).process[location].maxClaimsAllowed[claim]){
+					(*resourceTable).process[location].resourceRequest = claim; //save resource requested
+					message.mesg_type = 5;
 					message.RTLocation = location;
 					message.release = 0;
+					message.request = 1;
+					message.pid = myPID;
 
+					printf("REquested %d\n", (*resourceTable).process[location].resourceRequest);
+					printf("message type is%d\n", message.mesg_type);
+					printf("%d\n", message.RTLocation);
+					printf("%d\n", message.release);
 					if(msgsnd(messageBoxID, &message, sizeof(message), 0) == -1){
 						perror("User: failed to send message");
+					//prevent user from requesting more resources until its granted previous request
+					(*resourceTable).process[location].blocked = true;
+				//	break;
 					}
 				} else {
-					message.mesg_type = getppid();
-					message.request = 0;
+					message.mesg_type = 5;
 					message.RTLocation = location;
 					message.release = 0;
-					
-					if(msgsnd(messageBoxID, &message, sizeof(message), 0) == -1){
-						perror("User: failed to send message");
-					}
+					message.request = 0;
 				}
 			} else {	
 				//else block select a resource to release
 				//find a resource to release	
 				//sets message.release to the resource to release. returns -1 if no resources are found
-				message.release = FindResource(location, 0);
+				int releasedResource = findResource(location, 0);
 				
-				if (message.release >= 0){
-					message.mesg_type = getpid();
+				if (releasedResource >= 0){
+					(*resourceTable).process[location].resourceRelease = releasedResource;
+					message.mesg_type = 5;
 					message.RTLocation = location;
 					message.request = 0;
+					message.release = releasedResource;
+					message.pid = myPID;
 					if(msgsnd(messageBoxID, &message, sizeof(message), 0) ==  -1){
 						perror("oss: failed to send message to user");
 					}
@@ -123,6 +138,10 @@ int main (int argc, char *argv[]){
 					printf("No resources to release");	
 				}	
 			}			
+	
+	msgrcv(messageBoxID, &message, sizeof(message), myPID, IPC_NOWAIT);
+	requestWasGranted = message.granted;
+	} //end inner while loop
 	} //end outer while loop
 
 } //end main
@@ -137,11 +156,10 @@ int findResource(int allocated[][maxResources], int location){
 			return -1;
 		}
 	}	
-
 }
 */
 
-int FindResource(int location, int value){
+int findResource(int location, int value){
 	int i = 0;
 
 	while (i < maxProcesses && (*resourceTable).allocated[location][i] != value) ++i;
